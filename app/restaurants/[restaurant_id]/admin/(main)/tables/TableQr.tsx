25d3bf0f7cd5f9f5b156ad5fa6b465e226e0b4b4
dangdzone <nguyenvandang.co@gmail.com>
2024-01-02 23:20:15 +0700

    Update TableQr

--- app/restaurants/[restaurant_id]/admin/(main)/tables/TableQr.tsx
+++ app/restaurants/[restaurant_id]/admin/(main)/tables/TableQr.tsx
@@ -27,7 +27,7 @@ export const TableQr = ({ table }: TableQr) => {
                 size={80}
                 level='L'
                 style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-                value={`https://menu-v1.vercel.app/restaurants/${r.id}/tables/${table.id}`}
+                value={`https://menudientu.vercel.app/restaurants/${r.id}/tables/${table.id}`}
                 viewBox={`0 0 256 256`}
             />
             <HStack justifyContent='center' w='full'>
