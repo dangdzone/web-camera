50973ba6b120856610fce0370f644b689a88d441
dangdzone <nguyenvandang.co@gmail.com>
2024-05-18 18:40:58 +0700

    Update main page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,6 +1,6 @@
 'use client'
-import { HStack, SimpleGrid, Text, VStack } from "@chakra-ui/layout";
-import { Image } from "@chakra-ui/react";
+import { HStack, SimpleGrid, Stack, Text, VStack } from "@chakra-ui/layout";
+import { Image, Skeleton, Spinner } from "@chakra-ui/react";
 import { useCollectionData } from "@livequery/react";
 import { Brand, Category, Product } from "@/type";
 import { ProductItemBox } from "@/components/box/ProductItemBox";
@@ -18,29 +18,55 @@ export default function MainPage() {
                 <Image borderRadius='5px' src='https://cdn2.cellphones.com.vn/insecure/rs:fill:595:100/q:80/plain/https://dashboard.cellphones.com.vn/storage/r50.png' />
                 <Image borderRadius='5px' src='https://cdn2.cellphones.com.vn/insecure/rs:fill:595:100/q:80/plain/https://dashboard.cellphones.com.vn/storage/r100.png' />
             </SimpleGrid>
-            <SimpleGrid w='full' spacing='2' columns={[2, 3, 5, 6, 7, 8]}>
-                {
-                    $categories.items.map(category => (
-                        <CategoryItemBox category={category} />
-                    ))
-                }
-            </SimpleGrid>
-            <HStack w='full'>
-                {
-                    $brands.items.map(brand => (
-                        <Text
-                            px='4' py='2'
-                            key={brand.id}
-                            borderRadius='10px'
-                            border='1px' borderColor='blackAlpha.100'
-                            bg='blackAlpha.50'
-                            fontWeight='600'
-                        >
-                            {brand.name}
-                        </Text>
-                    ))
-                }
-            </HStack>
+            <Stack w='full' spacing='5'>
+                <Text fontWeight='700'>Danh mục</Text>
+                <SimpleGrid w='full' spacing='2' columns={[2, 3, 5, 6, 7, 8]}>
+                    {
+                        $categories.items.map(category => (
+                            <CategoryItemBox key={category.id} category={category} />
+                        ))
+                    }
+                </SimpleGrid>
+            </Stack>
+            <Stack w='full' spacing='5'>
+                <Text fontWeight='700'>Thương hiệu</Text>
+                <HStack w='full'>
+                    <Text
+                        px='4' py='2'
+                        borderRadius='10px'
+                        border='1px' borderColor='blackAlpha.100'
+                        bg={!$products.filters.brand_id ? 'blue.200' : 'blackAlpha.50'}
+                        fontWeight='600'
+                        cursor='pointer'
+                        onClick={() => $products.filter({
+                            ...$products.filters,
+                            brand_id: undefined
+                        })}
+                    >
+                        Tất cả
+                    </Text>
+                    {
+                        $brands.items.map(brand => (
+                            <Text
+                                px='4' py='2'
+                                key={brand.id}
+                                borderRadius='10px'
+                                border='1px'
+                                borderColor='blackAlpha.100'
+                                bg={brand.id == $products.filters.brand_id ? 'blue.200' : 'blackAlpha.50'}
+                                fontWeight='600'
+                                cursor='pointer'
+                                onClick={() => $products.filter({
+                                    ...$products.filters,
+                                    brand_id: brand.id
+                                })}
+                            >
+                                {brand.name}
+                            </Text>
+                        ))
+                    }
+                </HStack>
+            </Stack>
             <SimpleGrid w='full' spacing='4' columns={[1, 2, 3, 4]}>
                 {
                     $products.items.map(product => (
@@ -48,6 +74,9 @@ export default function MainPage() {
                     ))
                 }
             </SimpleGrid>
+            <VStack w='full' spacing='5'>
+                {$products.loading && <Spinner color="teal.500" size='lg' />}
+            </VStack>
         </VStack>
     )
 }
