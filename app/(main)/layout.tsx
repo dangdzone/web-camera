947957b8a835771b4e30a07a8046447158f9ccb8
dangdzone <nguyenvandang.co@gmail.com>
2024-06-09 19:07:59 +0700

    Update ScrollToTop

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,27 +2,29 @@
 import { Stack, VStack } from "@chakra-ui/layout";
 import { ReactNode } from "react";
 import { Topbar } from "./Topbar";
+import { ScrollToTop } from "@/components/common/ScrollToTop";
 
 export default function MainLayout({ children }: { children: ReactNode }) {
+
     return (
         <VStack
             h='100vh'
             spacing='0'
-            sx={{
-                "::-webkit-scrollbar": { w: { base: 'none', md: '2' } },
-                '&::-webkit-scrollbar-thumb': {
-                    borderRadius: '5',
-                    bg: 'blackAlpha.300'
-                },
-                "::-webkit-scrollbar-track": {
-                    background: 'white'
-                },
-                "::-webkit-scrollbar-thumb:hover": {
-                    bg: 'blackAlpha.400'
-                }
-            }}
-            overflowY='auto'
-            overflowX='hidden'
+        // sx={{
+        //     "::-webkit-scrollbar": { w: { base: 'none', md: '2' } },
+        //     '&::-webkit-scrollbar-thumb': {
+        //         borderRadius: '5',
+        //         bg: 'blackAlpha.300'
+        //     },
+        //     "::-webkit-scrollbar-track": {
+        //         background: 'white'
+        //     },
+        //     "::-webkit-scrollbar-thumb:hover": {
+        //         bg: 'blackAlpha.400'
+        //     }
+        // }}
+        // overflowY='auto'
+        // overflowX='hidden'
         >
             <VStack minH='60px' w='full' bg='linear-gradient(to right, #7f00ff, #e100ff)' boxShadow='sm' position='sticky' top='0px' zIndex='999'>
                 <VStack w='full' maxW='6xl' px='4'>
@@ -35,11 +37,11 @@ export default function MainLayout({ children }: { children: ReactNode }) {
                 maxW='6xl'
                 flexDirection='row'
                 spacing='5'
-                px={{base: '2', md: '4'}}
+                px={{ base: '2', md: '4' }}
             >
-                {/* <NavLink /> */}
                 {children}
             </Stack>
+            <ScrollToTop />
         </VStack>
     )
 }
---
+++ components/common/ScrollToTop.tsx
@@ -0,0 +1,42 @@
+import { Button, HStack, Text, VStack } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { FiChevronUp } from "react-icons/fi";
+
+export const ScrollToTop = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleVisibility = () => {
+        if (window.pageYOffset > 300) {
+            setIsVisible(true);
+        } else {
+            setIsVisible(false);
+        }
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", toggleVisibility);
+        return () => window.removeEventListener("scroll", toggleVisibility);
+    }, []);
+
+    return (
+        <HStack w='full' maxW='6xl' justifyContent='flex-end'>
+            {
+                isVisible && (
+                    <Button borderRadius='10px' onClick={scrollToTop} zIndex='999' bottom='10px' position="fixed" p='1' colorScheme='red'>
+                        <VStack spacing='0'>
+                            <FiChevronUp />
+                            <Text fontSize='10px'>Lên đầu</Text>
+                        </VStack>
+                    </Button>
+                )
+            }
+        </HStack>
+    );
+};
