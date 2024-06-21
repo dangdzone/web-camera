228943336d41e581c4279fe8ef467a2192d6e616
dangdzone <nguyenvandang.co@gmail.com>
2024-06-21 09:53:05 +0700

    Update tabs admin page

--- app/(main)/admin/page.tsx
+++ app/(main)/admin/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Tabs, TabList, Tab, TabPanels, TabPanel, TabIndicator, HStack, VStack } from "@chakra-ui/react";
+import { Tabs, TabList, Tab, TabPanels, TabPanel, TabIndicator, HStack, VStack, SimpleGrid } from "@chakra-ui/react";
 import { StatisticalPage } from "./statistical/StatisticalPage";
 import CategoryProductPage from "./category-product/CategoryProductPage";
 import { ResolutionPage } from "./resolution/ResolutionPage";
@@ -36,6 +36,13 @@ export default function AdminPage() {
         setSelectedTabIndex(index);
     };
 
+    const css_tab = {
+        _selected: {
+            bg: '#CF00FD',
+            color: 'white'
+        }
+    }
+
     return is_editor && (
         <VStack w='full' spacing='3' py='5'>
             <DirectionalLink directional={[
@@ -43,32 +50,31 @@ export default function AdminPage() {
                 { name: 'Admin', href: '' },
             ]} />
             <Tabs w='full' variant='unstyled' position='relative' index={selectedTabIndex} onChange={handleTabChange}>
-                <TabList  w='full' minW='10px' overflowX={{ base: 'scroll', md: 'auto' }}>
-                    <Tab fontWeight='600' whiteSpace='nowrap'>Thống kê</Tab>
-                    <Tab fontWeight='600' whiteSpace='nowrap'>Đơn hàng</Tab>
-                    <Tab fontWeight='600' whiteSpace='nowrap'>Danh mục & sản phẩm</Tab>
-                    <Tab fontWeight='600' whiteSpace='nowrap'>Độ phân giải</Tab>
-                    <Tab fontWeight='600' whiteSpace='nowrap'>Thương hiệu</Tab>
-                    <Tab fontWeight='600' whiteSpace='nowrap'>Hệ thống cửa hàng</Tab>
+                <TabList w='full' minW='10px' overflowX={{ base: 'scroll', md: 'auto' }}>
+                    <Tab fontWeight='600' whiteSpace='nowrap' borderRadius='10px' mr='2' {...css_tab}>Thống kê</Tab>
+                    <Tab fontWeight='600' whiteSpace='nowrap' borderRadius='10px' mr='2' {...css_tab}>Đơn hàng</Tab>
+                    <Tab fontWeight='600' whiteSpace='nowrap' borderRadius='10px' mr='2' {...css_tab}>Danh mục & sản phẩm</Tab>
+                    <Tab fontWeight='600' whiteSpace='nowrap' borderRadius='10px' mr='2' {...css_tab}>Độ phân giải</Tab>
+                    <Tab fontWeight='600' whiteSpace='nowrap' borderRadius='10px' mr='2' {...css_tab}>Thương hiệu</Tab>
+                    <Tab fontWeight='600' whiteSpace='nowrap' borderRadius='10px' {...css_tab}>Hệ thống cửa hàng</Tab>
                 </TabList>
-                <TabIndicator mt='-1.5px' height='3px' bg='teal.500' borderRadius='full' />
                 <TabPanels p='0'>
-                    <TabPanel py='7' px={{base: '2', md: '4'}}>
+                    <TabPanel py='7' px={{ base: '2', md: '4' }}>  
                         <StatisticalPage />
                     </TabPanel>
-                    <TabPanel py='7' px={{base: '2', md: '4'}}>
+                    <TabPanel py='7' px={{ base: '2', md: '4' }}>
                         <OrderPage />
                     </TabPanel>
-                    <TabPanel py='7' px={{base: '2', md: '4'}}>
+                    <TabPanel py='7' px={{ base: '2', md: '4' }}>
                         <CategoryProductPage />
                     </TabPanel>
-                    <TabPanel py='7' px={{base: '2', md: '4'}}>
+                    <TabPanel py='7' px={{ base: '2', md: '4' }}>
                         <ResolutionPage />
                     </TabPanel>
-                    <TabPanel py='7' px={{base: '2', md: '4'}}>
+                    <TabPanel py='7' px={{ base: '2', md: '4' }}>
                         <BrandPage />
                     </TabPanel>
-                    <TabPanel py='7' px={{base: '2', md: '4'}}>
+                    <TabPanel py='7' px={{ base: '2', md: '4' }}>
                         <StorePage />
                     </TabPanel>
                 </TabPanels>
