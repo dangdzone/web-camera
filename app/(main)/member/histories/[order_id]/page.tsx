ed4d6a5df681efd9f494fe62cee5e7c9f2cfde92
dangdzone <nguyenvandang.co@gmail.com>
2024-06-23 11:54:19 +0700

    Update histories/order_id

--- app/(main)/member/histories/[order_id]/page.tsx
+++ app/(main)/member/histories/[order_id]/page.tsx
@@ -4,7 +4,7 @@ import { useFirebaseUserContext } from "@/hooks/useFirebaseUser"
 import { OrderStatusMap } from "@/text"
 import { Order } from "@/type"
 import { Divider, HStack, Stack, Text, VStack } from "@chakra-ui/layout"
-import { Button } from "@chakra-ui/react"
+import { Button, Select } from "@chakra-ui/react"
 import { useDocumentData } from "@livequery/react"
 import dayjs from "dayjs"
 import Link from "next/link"
@@ -15,6 +15,8 @@ import { useEffect, useRef, useState } from "react"
 import { FaRegFilePdf } from "react-icons/fa6"
 import { jsPDF } from 'jspdf';
 import html2canvas from 'html2canvas';
+import { SmartQueryItem } from "@livequery/client"
+import { VietQRModal } from "@/app/(main)/cart/payment/[order_id]/VietQRModal"
 
 export default function OrderIdPage(props: {
     params: {
@@ -22,14 +24,37 @@ export default function OrderIdPage(props: {
     }
 }) {
     const { fuser } = useFirebaseUserContext()
-    const { item: $order_item } = useDocumentData<Order>(fuser && `customers/${fuser.uid}/orders/${props.params.order_id}`)
-    const status_order = Object.entries(OrderStatusMap).filter(([status,]) => status == $order_item?.status).map(([status_id, { color, name }]) => [{ name, color }])[0]
+    const { item: order } = useDocumentData<Order>(fuser && `customers/${fuser.uid}/orders/${props.params.order_id}`)
+    const status_order = Object.entries(OrderStatusMap).filter(([status,]) => status == order?.status).map(([status_id, { color, name }]) => [{ name, color }])[0]
 
-    const total_pay = $order_item?.pay + $order_item?.shipping_fee
+    const [type, set_type] = useState<string>('momo')
+    const [active_vietqr, set_active_vietqr] = useState<boolean | SmartQueryItem<Order>>(false)
+    const pay = async () => {
+        try {
+            if (type == 'vietqr') {
+                set_active_vietqr(true)
+            } else {
+                const result = await order.__trigger('pay', { type }) as {
+                    data: {
+                        item: {
+                            url: string
+                        }
+                    }
+                }
+                if (result?.data.item.url) {
+                    window.location.href = result?.data.item.url
+                }
+            }
+        } catch (e) {
+            throw new Error('Đã xảy ra lỗi, vui lòng thử lại !')
+        }
+    }
+
+    const total_pay = order?.pay + order?.shipping_fee
     const statistical = [
-        { name: 'Số lượng sản phẩm', value: $order_item?.amount, unit: '' },
-        { name: 'Tiền hàng (tạm tính)', value: $order_item?.pay, unit: 'đ' },
-        { name: 'Phí vận chuyển', value: $order_item?.shipping_fee, unit: 'đ' },
+        { name: 'Số lượng sản phẩm', value: order?.amount, unit: '' },
+        { name: 'Tiền hàng (tạm tính)', value: order?.pay, unit: 'đ' },
+        { name: 'Phí vận chuyển', value: order?.shipping_fee, unit: 'đ' },
     ]
 
     const [time, setTime] = useState<string | null>(null);
@@ -51,12 +76,16 @@ export default function OrderIdPage(props: {
             const pdfWidth = pdf.internal.pageSize.getWidth();
             const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
             pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
-            pdf.save(`donhang_${$order_item.code}.pdf`);
+            pdf.save(`donhang_${order.code}.pdf`);
         }
     };
 
-    return (
+
+    return fuser && (
         <Stack w='full' spacing='5'>
+            {
+                active_vietqr && order.status == 'created' && <VietQRModal onClose={() => set_active_vietqr(false)} order={order} />
+            }
             <HStack w='full' justifyContent='space-between'>
                 <HStack spacing='5'>
                     <Link href={'/member/histories'}>
@@ -65,7 +94,7 @@ export default function OrderIdPage(props: {
                     <Text fontWeight='600' fontSize='18px'>Chi tiết đơn hàng</Text>
                 </HStack>
                 {
-                    $order_item?.status == 'paid' && (
+                    order?.status == 'paid' && (
                         <Button size='sm' borderRadius='10px' variant='outline' leftIcon={<FaRegFilePdf />} onClick={handleDownloadPdf}>Xuất pdf</Button>
                     )
                 }
@@ -75,7 +104,7 @@ export default function OrderIdPage(props: {
                     <HStack w='full' justifyContent='space-between'>
                         <HStack>
                             <Text fontSize='15px'>Mã đơn hàng:</Text>
-                            <Text fontWeight='600'>{$order_item?.code}</Text>
+                            <Text fontWeight='600'>{order?.code}</Text>
                         </HStack>
                         <Text fontWeight='500' color={status_order?.map(a => a.color)[0]}>{status_order?.map(a => a.name)[0]}</Text>
                     </HStack>
@@ -86,19 +115,19 @@ export default function OrderIdPage(props: {
                 </Stack>
                 <Stack w='full'>
                     {
-                        $order_item?.order_items.map((order_item, i) => (
+                        order?.order_items.map((order_item, i) => (
                             <OrderIdItem key={i} order_item={order_item} />
                         ))
                     }
                 </Stack>
                 <Stack w='full'>
                     <Text fontWeight='500'>Thông tin nhận hàng</Text>
-                    <ReceiverInfo address_id={$order_item?.address_id} />
+                    <ReceiverInfo address_id={order?.address_id} />
                 </Stack>
                 <Stack w='full'>
                     <Text fontWeight='500'>Ghi chú</Text>
                     <Stack w='full' border='1px' spacing='4' borderColor='blackAlpha.200' borderRadius='10px' p='4'>
-                        <Text>{$order_item?.note ? $order_item?.note : 'Không có'}</Text>
+                        <Text>{order?.note ? order?.note : 'Không có'}</Text>
                     </Stack>
                 </Stack>
                 <VStack w='full' border='1px' borderColor='blackAlpha.200' borderRadius='10px' p='4' spacing='4'>
@@ -118,10 +147,10 @@ export default function OrderIdPage(props: {
                 </VStack>
             </Stack>
             {
-                $order_item?.status == 'created' && (
+                order?.status == 'created' && (
                     <VStack
                         p='4' w='full'
-                        spacing='5'
+                        spacing='4'
                         boxShadow='rgba(99, 99, 99, 0.2) 0px 2px 8px 0px'
                         borderRadius='10px'
                         pos='sticky'
@@ -131,7 +160,20 @@ export default function OrderIdPage(props: {
                         border='1px'
                         borderColor='blackAlpha.200'
                     >
-                        <Button w='full' borderRadius='10px' colorScheme='red'>Thanh toán</Button>
+                        {
+                            order.status == 'created' && (
+                                <HStack w='full' flexDir={{base: 'column', md: 'row'}} spacing='4' justifyContent='space-between'>
+                                    <Text fontWeight='500' whiteSpace='nowrap'>Chọn phương thức thanh toán</Text>
+                                        <Select borderRadius='10px' w={{base: '100%', md: '60%'}} onChange={(e) => set_type(e.target.value)}>
+                                            <option value='momo'>Ví momo</option>
+                                            <option value='zalo'>Zalo pay</option>
+                                            <option value='ninepay'>9pay</option>
+                                            <option value='vietqr'>Chuyển khoản ngân hàng</option>
+                                        </Select>
+                                </HStack>
+                            )
+                        }
+                        <Button w='full' borderRadius='10px' colorScheme='red' onClick={pay}>Thanh toán</Button>
                     </VStack>
                 )
             }
